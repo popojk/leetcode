f3034ef4a5507d8dc347932d629c2e46f187f174
Alex Wu <c.alexwu@gmail.com>
2024-08-25 11:59:51 +0800

    fix 659

--- 659. Split Array into Consecutive Subsequences/solution.py
+++ 659. Split Array into Consecutive Subsequences/solution.py
@@ -22,20 +22,21 @@ class Solution:
                     return False
         return True
 
-    class SolutionB:
-        def isPossible(self, nums: List[int]) -> bool:
-            counter = collections.Counter(nums)  # 對數組中的元素進行計數
 
-            for i in sorted(counter.keys()):  # 對有不同值的元素排序後進行迭代
-                while counter[i] > 0:  # 當當前元素的計數大於0時，嘗試構建序列
-                    last = 0
-                    j = i
-                    k = 0
-                    while counter[j] >= last:  # 構建一個連續的子序列
-                        last = counter[j]
-                        counter[j] -= 1
-                        j += 1
-                        k += 1
-                    if k < 3:  # 如果構建的序列長度小於3，則返回 False
-                        return False
-            return True  # 如果所有元素都可以構建成長度至少為3的連續子序列，則返回 True
+class SolutionB:
+    def isPossible(self, nums: List[int]) -> bool:
+        counter = collections.Counter(nums)  # 對數組中的元素進行計數
+
+        for i in sorted(counter.keys()):  # 對有不同值的元素排序後進行迭代
+            while counter[i] > 0:  # 當當前元素的計數大於0時，嘗試構建序列
+                last = 0
+                j = i
+                k = 0
+                while counter[j] >= last:  # 構建一個連續的子序列
+                    last = counter[j]
+                    counter[j] -= 1
+                    j += 1
+                    k += 1
+                if k < 3:  # 如果構建的序列長度小於3，則返回 False
+                    return False
+        return True  # 如果所有元素都可以構建成長度至少為3的連續子序列，則返回 True
