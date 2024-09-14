8f590951040bc5ea8b763d83eb27d7aaa11100ff
Alex Wu <c.alexwu@gmail.com>
2024-09-14 11:16:42 +0800

    add dp solution to 45

--- 45. Jump Game II/solution.py
+++ 45. Jump Game II/solution.py
@@ -12,3 +12,17 @@ class Solution:
             far = farest
             jumps += 1
         return jumps
+
+
+class DPSolution:
+    def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 1:
+            return 0
+        dp = [0] * n
+        j = 0
+        for i in range(1, n):
+            while j + nums[j] < i:
+                j += 1
+            dp[i] = dp[j] + 1
+        return dp[-1]
