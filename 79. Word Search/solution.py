3cd4ad247fec524744c506245bc59975e4570f11
Alex Wu <c.alexwu@gmail.com>
2024-09-21 11:43:34 +0800

    amend 79

--- 79. Word Search/solution.py
+++ 79. Word Search/solution.py
@@ -34,3 +34,26 @@ class Solution:
                     if search(word, i, j, set()):
                         return True
         return False
+
+    class CleanSolution:
+        def exist(self, board: List[List[str]], word: str) -> bool:
+            def back_track(i, j, k):
+                if k == len(word):
+                    return True
+                if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or board[i][j] != word[k]:
+                    return False
+
+                temp = board[i][j]
+                board[i][j] = ''
+                # do back track
+                if back_track(i+1, j, k+1) or back_track(i-1, j, k+1) or back_track(i, j+1, k+1) or back_track(i, j-1, k+1):
+                    return True
+                # recover back track
+                board[i][j] = temp
+                return False
+
+            for i in range(len(board)):
+                for j in range(len(board[0])):
+                    if back_track(i, j, 0):
+                        return True
+            return False
