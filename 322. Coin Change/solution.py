142760f87599ab4e34f9a384cdb47c0a7ab3cb46
Alex Wu <c.alexwu@gmail.com>
2024-09-23 13:12:22 +0800

    amend 322

--- 322. Coin Change/solution.py
+++ 322. Coin Change/solution.py
@@ -1,3 +1,4 @@
+import math
 from typing import List
 
 
@@ -10,3 +11,19 @@ class Solution:
                 if i - coin >= 0:
                     min_coins[i] = min(min_coins[i], min_coins[i-coin] + 1)
         return min_coins[-1] if min_coins[-1] != amount+1 else -1
+
+
+class TopDownDPSolution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        def dfs(rem, cache):
+            if rem < 0:
+                return math.inf
+            if rem == 0:
+                return 0
+            if rem in cache[rem]:
+                return cache[rem]
+            cache[rem] = min(dfs(rem-x, cache) + 1 for x in coins)
+            return cache[rem]
+
+        ans = dfs(amount, {})
+        return -1 if ans == math.inf else ans
