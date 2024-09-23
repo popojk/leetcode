db7b3c00954a612ee9b3981ac994d29c30034754
Alex Wu <c.alexwu@gmail.com>
2024-09-23 13:00:20 +0800

    add 322

---
+++ 322. Coin Change/solution.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        min_coins = [amount+1] * (amount+1)
+        min_coins[0] = 0
+        for i in range(1, amount+1):
+            for coin in coins:
+                if i - coin >= 0:
+                    min_coins[i] = min(min_coins[i], min_coins[i-coin] + 1)
+        return min_coins[-1] if min_coins[-1] != amount+1 else -1
