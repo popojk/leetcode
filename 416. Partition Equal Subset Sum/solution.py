67e590e2e7bc717d4625169465369ce18d8bd869
Alex Wu <c.alexwu@gmail.com>
2024-10-08 11:34:16 +0800

    add optimize solution to 416

--- 416. Partition Equal Subset Sum/solution.py
+++ 416. Partition Equal Subset Sum/solution.py
@@ -12,3 +12,31 @@ class Solution:
             for curr in range(s, num-1, -1):
                 dp[curr] = dp[curr] or dp[curr-num]
         return dp[s//2]
+
+
+class OptimizeSolution:
+    def canPartition(self, nums: List[int]) -> bool:
+        # if sum is odd, return False
+        s = sum(nums)
+        if s % 2 != 0:
+            return False
+        dp = [True] + [False]*(s >> 1)
+        for num in nums:
+            for curr in range(s >> 1, num-1, -1):
+                dp[curr] = dp[curr] or dp[curr-num]
+        return dp[-1]
+
+
+class SecondOptimizeSolution:
+    def canPartition(self, nums: List[int]) -> bool:
+        # if sum is odd, return False
+        dp, s = set(), sum(nums)
+        if s % 2 != 0:
+            return False
+        for num in nums:
+            for curr in range(s >> 1, num-1, -1):
+                if curr not in dp and curr-num in dp:
+                    if curr == s >> 1:
+                        return True
+                    dp.add(curr)
+        return False
