f4138dc8f7491aba904b23548b6e5c7da9e91050
Alex Wu <c.alexwu@gmail.com>
2024-12-14 11:23:59 +0800

    fix 55 error

--- 55. Jump Game/solution.py
+++ 55. Jump Game/solution.py
@@ -3,12 +3,12 @@ from typing import List
 
 class DPSolution:
     def canJump(self, nums: List[int]) -> bool:
-        dp = [0] * len(nums)
+        dp = [False] * len(nums)
         dp[0] = True
 
-        for i in range(len(nums)):
-            for j in range(i-1, -1, -1):
-                if dp[j] and nums[j] >= (i-j):
+        for i in range(1, len(nums)):
+            for j in range(i - 1, -1, -1):
+                if dp[j] and nums[j] >= (i - j):
                     dp[i] = True
                     break
         return dp[-1]
